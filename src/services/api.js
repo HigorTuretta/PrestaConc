598d51a1e3375bc49169c08f632d5d1d0c20757e
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-10-13 16:51:08 -0300

    teste ngrok

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://prestaconc.onrender.com" //"https://prestaconc.onrender.com" // "http://localhost:3333"//
+    baseURL: "https://4bc9-191-36-207-135.ngrok-free.app" //"https://prestaconc.onrender.com" // "http://localhost:3333"// https://4bc9-191-36-207-135.ngrok-free.app
 })
