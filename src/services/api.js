c305e256f6f222821de69ea702ce02b876ffccfb
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2024-03-16 16:03:50 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://prestaconc.onrender.com" //"https://prestaconc.onrender.com" // "http://localhost:3333"// https://4bc9-191-36-207-135.ngrok-free.app
+    baseURL: " https://64d6-191-36-207-20.ngrok-free.app" //"https://prestaconc.onrender.com" // "http://localhost:3333"// https://4bc9-191-36-207-135.ngrok-free.app
 })
